32ea215d659574c4c407aca70e503bc9617d62cd
sagar\sagar_2621 <sagarb@triconinfotech.com>
2018-07-17 12:16:35 +0530

    header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,30 @@
-import React from 'react';  
+import React, {Component} from 'react';  
 import {BrowserRouter, Route} from 'react-router-dom';
-
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 import Header from './Header';
 const Dashboard = () => <div>Dashboard</div>
 const SurverNew = () => <div>SurverNew</div>
 const Landing = () => <div>Landing</div>
 
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <div>   
-                    <Header/>
-                    <Route exact={true} path="/" component={Landing}/>
-                    <Route exact={true} path="/surveys" component={Dashboard}/>
-                    <Route path="/surveys/new" component={SurverNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
-    )
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return(
+            <div>
+                <BrowserRouter>
+                    <div>   
+                        <Header/>
+                        <Route exact={true} path="/" component={Landing}/>
+                        <Route exact={true} path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurverNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
 };
 
-export default App;
+export default  connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+    renderCotent(){
+        switch(this.props.auth){
+            case null: 
+                return "Still deciding";
+            case false:
+                return "im loggedout";
+            default:
+                return "im loggedin";
+        }
+    }
     render() {
+        console.log(this.props);
         return (
         <nav>
         <div className="nav-wrapper">
         <a href="#" className="brand-logo">Logo</a>
         <ul id="nav-mobile" className="right">
-            <li><a href="sass.html">Login with google</a></li>
+            <li><a href="sass.html">{this.renderCotent()}</a></li>
         </ul>
         </div>
     </nav>
         )
     }
 }
+
+function mapStateToProps(state){
+    return { auth: state.auth}
+}
+
+export default connect(mapStateToProps)(Header);
