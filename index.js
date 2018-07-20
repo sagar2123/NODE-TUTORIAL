3e7dfae7a5cd635014b8945fdf0cc50e558b26cf
TRICONHOME\sagarb <sagarb@triconinfotech.com>
2018-07-20 19:47:48 +0530

    Billing done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30*24*60*60*1000,
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,18 @@
 const keys = require("../config/keys");
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post("/api/stripe", (req, res) => {
-        console.log(req.body);
+    app.post("/api/stripe", requireLogin, async (req, res) => {
+        const charges = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '5$ for 5 credits',
+            source: req.body.id
+        })
+        
+        req.user.credits += 5;
+        const user = await req.user.save();
+        res.send(user); 
     })
 }
